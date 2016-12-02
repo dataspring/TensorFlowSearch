13b88b71297f0422fd502757e9fc107466b35416
osboxes.org <dataspring.avp@gmail.com>
2016-12-02 06:20:46 +0000

    Crawler and  Google Cloud Settings

--- settings.py
+++ settings.py
@@ -1,11 +1,47 @@
-import sys
-USER = "ubuntu"
-HOST = "52.90.19.2"
-AWS = sys.platform != 'darwin'
-private_key =  "~/.ssh/cs5356"
+import sys, time
+#USER = "ubuntu"
+USER = "nachi"  #local user
+#USER = "sgd.cloud"  #gce user
+#HOST = "52.90.19.2"
+#HOST = "192.168.1.117" #home wifi
+HOST = "198.162.10.181" #office ether
+#HOST = "104.199.121.106"  #gce public
+
+#AWS = sys.platform != 'darwin'
+AWS = sys.platform != 'linux2'
+#private_key =  "~/.ssh/cs5356"
+#private_key =  "/home/osboxes/nachi/keys/id_rsa"
+private_key =  "/home/osboxes/Vs-ver2/keys/id_rsa"
 CONFIG_PATH = __file__.split('settings.py')[0]
 
 BUCKET_NAME = "aub3visualsearch"
 PREFIX = "nyc"
-INDEX_PATH = "/mnt/nyc_index/"
-DATA_PATH ="/mnt/nyc_images/"
+# INDEX_PATH = "/mnt/nyc_index/"
+# DATA_PATH ="/mnt/nyc_images/"
+
+
+INDEX_PATH = "/home/osboxes/Vs-ver2/carousell/index/"
+DATA_PATH ="/home/osboxes/Vs-ver2/carousell/images/"
+DONE_DATA_PATH ="/home/osboxes/Vs-ver2/carousell/done/"
+SQLITE_PATH = "/home/osboxes/Vs-ver2/sqllite3/"
+
+# INDEX_PATH = "/home/sgd.cloud/deep/carousell/index/"
+# DATA_PATH ="/home/sgd.cloud/deep/carousell/images/"
+# DONE_DATA_PATH ="/home/sgd.cloud/deep/carousell/done/"
+# SQLITE_PATH = "/home/sgd.cloud/deep/carousell/sqllite3/"
+
+RESULT_STEPS = 100
+MAX_ITER = 50
+MAX_COLLECTION = 12
+BATCH_SIZE = 500
+
+
+START_COLLECTION = 4
+ONETIME_COUNTSTART = True
+ITER_START_COUNT = 41
+
+INDEX_RUN = str(time.time())
+CRAWL_RUN = str(time.time())
+SQLDB_NAME = 'carousellv11.sqlite'
+
+#for query : ;path=/products/collections/4/;query={"count":100,"start":4101}
