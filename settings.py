04191c928205f1d956c6bd881098c02a62e8ae3c
osboxes.org <dataspring.avp@gmail.com>
2016-12-04 07:46:23 +0000

    GetIP changes

--- settings.py
+++ settings.py
@@ -51,21 +51,21 @@ def getusername():
     return getpass.getuser()
 
 
-# def getip():
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     try:
-#         # doesn't even have to be reachable
-#         s.connect(('10.255.255.255', 0))
-#         IP = s.getsockname()[0]
-#     except:
-#         IP = '127.0.0.1'
-#     finally:
-#         s.close()
-#     return IP
+def getip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 0))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
-def getlp():
-    return '127.0.0.1'       
+# def getlp():
+#     return '127.0.0.1'       
 
 USER = getusername
 HOST = getlp
